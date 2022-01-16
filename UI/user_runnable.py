941264ecc85e2167e450698369f8194c5ed40fd6
ikamino <elee1820@gmail.com>
2022-01-16 02:13:21 -0800

    init functions for running

--- UI/user_runnable.py
+++ UI/user_runnable.py
@@ -1,4 +1,28 @@
 def init():
     print("Welcome to indecisive, a program designed to help you places to eat near you, depending on how you feel!")
-
+    running = 1
     
+
+    while running: 
+        menu()
+        running = 0 
+
+
+def menu(): 
+    print('Please input what you would like to do. Type \t help \t to get a list of commands')
+
+    user_input = input() 
+    user_input.lower
+    commands(user_input)
+    
+
+
+def commands(user_input): 
+    if user_input == help: 
+        help()
+
+def help():
+    print('Here are the available functions to you \n exit: exit the program \nhelp')
+    
+
+init()

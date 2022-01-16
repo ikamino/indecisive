896c2d99be3d4055a59759bd2aaf694307127671
Isaaczhu9877 <opisaac24@gmail.com>
2022-01-16 05:31:10 -0800

    test file uploaded to tests

---
+++ Tests/testmap1.py
@@ -0,0 +1,18 @@
+import googlemaps as gm
+from geopy.geocoders import Nominatim
+
+maps = gm.Client(key = 'AIzaSyCqpnuB4GgssSCxpbmGrGWYSyzGcOtgleo')
+        # url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(location) +'?format=json'
+        # response = requests.get(url).json()
+        # lat = response[0]["lat"]
+        # long = response[0]["lon"]
+geolocator = Nominatim(user_agent="http")
+location = geolocator.geocode('6279 Eagles Dr Vancouver BC')
+# we need address, city, and province
+lat_long = str(location.latitude) + "," + str(location.longitude)
+result = maps.places_nearby(location = lat_long, radius = 40000, open_now = False, type = "Cafe")
+for place in result['results']:
+            my_place_id = place['place_id']
+            my_fields = ['name', 'formatted_phone_number', 'type']
+            place_details = maps.place(place_id = my_place_id, fields = my_fields)
+            print(place_details)

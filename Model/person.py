18210c414da4882ba7c418ad21d6679c4d161277
ikamino <elee1820@gmail.com>
2022-01-15 23:56:06 -0800

    commiting person classe

--- Model/person.py
+++ Model/person.py
@@ -1,8 +1,8 @@
 class Person:
     price_range = 0 # one of [1]
-    location = '' #
-    feeling = ''
-    name = ''
+    location = '' # address 
+    feeling = '' # a word out of a list of possible choices
+    name = '' #
 
     def __init__(self, name, price_range, location, ):
             # Assign the argument to the instance's name attribute

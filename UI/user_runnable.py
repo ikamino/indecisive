3d05c2fa446b4783a539b0c555a04159f492094d
ikamino <elee1820@gmail.com>
2022-01-16 02:41:44 -0800

    finished all the preliminary print helper functions to be ran off of main

--- UI/user_runnable.py
+++ UI/user_runnable.py
@@ -1,11 +1,15 @@
+from turtle import hideturtle
+
+running = 1
 def init():
+    global running
     print("Welcome to indecisive, a program designed to help you places to eat near you, depending on how you feel!")
     running = 1
     
 
     while running: 
-        menu()
-        running = 0 
+        menu(running)
+   
 
 
 def menu(): 
@@ -13,16 +17,28 @@ def menu():
 
     user_input = input() 
     user_input.lower
+    # commands(user_input)
     commands(user_input)
-    
 
 
 def commands(user_input): 
-    if user_input == help: 
+    if user_input == "help": 
         help()
+    elif user_input == "decisions":
+        decisions()
+
+    elif user_input == "exit": 
+        exit_program()
 
 def help():
-    print('Here are the available functions to you \n exit: exit the program \nhelp')
+    print('Here are the available functions to you \nhelp: display commands \ndecisions: go through the steps to satisfy your cravings for the day. \n exit: exit the program')
     
+
+def exit_program():
+    global running
+    running = 0
+
+def decisions(): 
+    print("We're glad to help you find what to eat, and where, near you!")
 
 init()

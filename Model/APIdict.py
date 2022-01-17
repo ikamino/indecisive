1f4257b80e95c9a39db4b40b8d7f0faa2b883ca7
Isaaczhu9877 <opisaac24@gmail.com>
2022-01-16 23:00:01 -0800

    Deleted Key

--- Model/APIdict.py
+++ Model/APIdict.py
@@ -5,7 +5,7 @@ from geopy.geocoders import Nominatim
 
 def rest_info_list(location):
     place_data = []
-    maps = gm.Client(key = 'AIzaSyCqpnuB4GgssSCxpbmGrGWYSyzGcOtgleo')
+    maps = gm.Client(key = '!!!PASTE APIKEY HERE!!!')
         # url = 'https://nominatim.openstreetmap.org/search/' + urllib.parse.quote(location) +'?format=json'
         # response = requests.get(url).json()
         # lat = response[0]["lat"]
